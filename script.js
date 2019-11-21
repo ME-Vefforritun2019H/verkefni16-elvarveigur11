b496cf2bf802a69cae2c4c21459ccbc073559fba
elvar.veigur <elvarveigur00@gmail.com>
2019-11-21 15:37:04 +0000

    Búinn

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let table = 3;
+let operator = "addition";
+let i = 1;
+let msg = "";
+
+if(operator === "addition") {
+    while (i<11) {
+        msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+        // console.log(i);
+        i++;
+    }
+}
+
+
+let el = document.getElementById("blackboard");
+el.innerHTML = msg;
 
